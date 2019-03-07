e182d971c61454936aedd8d78611fe4154e6df0e
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-03-07 11:52:06 -0600

    Stack Hospital class complete

--- src/StackHospital.java
+++ src/StackHospital.java
@@ -1,28 +1,46 @@
+import java.util.Deque;
+import java.util.LinkedList;
 
-public class StackHospital<PatientType> extends Hospital
+
+public class StackHospital<PatientType> extends Hospital<PatientType>
 {
+    private Deque<PatientType> hospital;
+    public StackHospital() {
+        hospital = new LinkedList<PatientType>();
+    }
+    public void addPatient(PatientType patient)
+    {
+       hospital.addFirst(patient);
+    }
 
-public void addPatient(PatientType patient) {
-        
+    public PatientType nextPatient()
+    {
+          return hospital.peekFirst();
     }
-    
-    public PatientType nextPatient() {
-        
+
+    public PatientType treatNextPatient()
+    {
+           return hospital.removeFirst();
     }
-    
-    public PatientType treatNextPatient() {
-        
+
+    public int numPatients()
+    {
+        return hospital.size();
     }
-    
-    public int numPatients() {
-        
+
+    public String hospitalType()
+    {
+        //Format might be incorrect when submitted.
+        return "Stack Hospital";
     }
-    
-    public String hospitalType() {
-        
-    }
-    
-    public String allPatientInfo() {
-        
+
+    public String allPatientInfo()
+    {
+        String all = "";
+        for(int i = 0; i < hospital.size(); i++) {
+            //Check this later
+           all.concat(((LinkedList<PatientType>) hospital).get(i).toString());
+        }
+        return all;
     }
 }
