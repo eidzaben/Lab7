d99dfdf5e3a8dee0593c3cb7c3bdabed915333b7
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-03-08 13:24:04 -0600

    Sick and Healthy person class constructors and toStrings complete

--- src/HealthyPerson.java
+++ src/HealthyPerson.java
@@ -1,16 +1,28 @@
 
 public class HealthyPerson extends Person
 {
+    private String name;
+    private int age;
+    private String reason;
 
-    public Person(String name, int age, int severity) {
-        
+    public HealthyPerson(String name, int age, String reason)
+    {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+        this.reason = reason;
     }
-    
-    protected int compareToImpl(Person p) {
-        
+
+    protected int compareToImpl(Person p)
+    {
+         /*  if(this.compareTo(p) == 0) {
+               return 0;
+           } else if ()
+           */
     }
-    
-    public String toString() {
-        
+
+    public String toString()
+    {
+            return String.format("%s In for %s", super.toString(), reason);
     }
 }
--- src/SickPerson.java
+++ src/SickPerson.java
@@ -2,15 +2,22 @@
 public class SickPerson extends Person
 {
 
-    public Person(String name, int age, int severity) {
-        
+    private String name;
+    private int age;
+    private int severity;
+    
+    public SickPerson(String name, int age, int severity) {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+        this.severity = severity;
     }
     
-    protected int compareToImpl(Person p) {
+    protected int compareToImpl(Person o) {
         
     }
     
     public String toString() {
-        
+        return String.format("s Severity level %d", super.toString(), severity);
     }
 }
