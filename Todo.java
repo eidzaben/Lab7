e76a96c91ce138825d2c08ce2a8263b5047f6cb0
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-03-12 14:57:28 -0500

    Todo added

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-12
+OU ID_______________: 113334522
+OU EMAIL____________: eid.s.zaben-1@ou.edu
+
+Tasks:
+1. Complete the priority queue hospital
+2. Complete the queue hospital
+3. Complete the stack hospital
+4. Complete sick and healthy person classes
+5. Write tests for the code
+
+Estimated Time (minutes):
+1. 20
+2. 20
+3. 20
+4. 40
+5. 30
+
+Actual Time (minutes):
+1. 20
+2. 10
+3. 25
+4. 70
+5. 40
+*/}
