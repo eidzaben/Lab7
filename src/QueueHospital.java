7807240d82e534188cb112b50b6051a61d6775ff
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-03-12 21:36:11 -0500

    Queue Hospital comments added

--- src/QueueHospital.java
+++ src/QueueHospital.java
@@ -1,48 +1,44 @@
-import java.util.AbstractList;
 import java.util.LinkedList;
-import java.util.ListIterator;
 import java.util.Queue;
 
 public class QueueHospital<PatientType> extends Hospital<PatientType>
 {
     private Queue<PatientType> hospital = new LinkedList<PatientType>();;
-
-    // ListIterator<String> listIterator;
+    //Constructor
     public QueueHospital()
     {
         hospital = new LinkedList<PatientType>();
     }
-
+    //Adds patient
     public void addPatient(PatientType patient)
     {
         hospital.add(patient);
     }
-
+    //Returns next patient
     public PatientType nextPatient()
     {
         return hospital.peek();
     }
-
+    //Treats next patient
     public PatientType treatNextPatient()
     {
         return hospital.remove();
     }
-
+    //Returns num of patients
     public int numPatients()
     {
         return hospital.size();
     }
-
+    //Returns hospital type
     public String hospitalType()
     {
         return "QueueHospital";
     }
-
+    //Returns patients info
     public String allPatientInfo()
     {
-
-        // Might cause issues
         String all = "";
+        //Concats all patients toStrings
         for (PatientType p : hospital)
         {
             all += p.toString();
