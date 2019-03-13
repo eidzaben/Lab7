13a04a5f2ff42ff7a84be88d229635ae033ace0e
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-03-12 21:33:44 -0500

    PQ and PQ Test comments added

--- src/PriorityQueueHospital.java
+++ src/PriorityQueueHospital.java
@@ -4,40 +4,43 @@ import java.util.PriorityQueue;
 public class PriorityQueueHospital<PatientType extends Comparable<PatientType>> extends Hospital<PatientType>
 {
     private PriorityQueue<PatientType> hospital;
-
+    //Constructor
     public PriorityQueueHospital()
     {
         hospital = new PriorityQueue<PatientType>();
     }
 
+    //Adds patient
     public void addPatient(PatientType patient)
     {
         hospital.add(patient);
     }
-
+    //Returns next patient
     public PatientType nextPatient()
     {
         return hospital.peek();
     }
-
+    //returns and removes next patient
     public PatientType treatNextPatient()
     {
         return hospital.poll();
     }
 
+    //Returns number of patients
     public int numPatients()
     {
         return hospital.size();
     }
-
+    //Returns hospital type
     public String hospitalType()
     {
-        return "PriorityQueueHospital";
+        return "PriorityQueueHospital"; 
     }
 
     public String allPatientInfo()
     {
         String all = "";
+        //For loop adds each patients toString()
         for (PatientType p : hospital)
         {
             all += p.toString();
---
+++ src/PriorityQueueHospitalTest.java
@@ -0,0 +1,38 @@
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PriorityQueueHospitalTest
+{
+
+    @Test
+    public void PQHTest()
+    {
+        //Priority Queue Test created
+        PriorityQueueHospital<Person> pqh = new PriorityQueueHospital<Person>();
+        
+        //Patients created
+        SickPerson sickBill = new SickPerson("Bill", 20, 6);
+        HealthyPerson healthyJan = new HealthyPerson("Jan", 8, "check up");
+        
+        //Patients added to hospital
+        pqh.addPatient(sickBill);
+        pqh.addPatient(healthyJan);
+        
+        //Checks hospital type and num of patients
+        Assert.assertEquals("PriorityQueueHospital", pqh.hospitalType());
+        Assert.assertEquals(2, pqh.numPatients());
+        //Checks patient info
+        Assert.assertEquals("Bill, a 20-year old. Severity level 6Jan, a 8-year old. In for check up",
+                pqh.allPatientInfo());
+        
+        //Checks next patient and treatNextPatient
+        Assert.assertEquals(sickBill, pqh.nextPatient());
+        Assert.assertEquals(sickBill, pqh.treatNextPatient());
+        
+        //Checks num of patients after one removed
+        Assert.assertEquals(1, pqh.numPatients());
+      
+    }
+}
